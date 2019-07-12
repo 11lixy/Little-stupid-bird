219da70df716cbc659edc1197efd00e6e3f73910
11lixy <51400074+11lixy@users.noreply.github.com>
2019-07-12 22:15:31 +0800

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,6 +1,17 @@
-cars = ['audi','bmw','subaru','toyota']
-for car in cars:
-	 if car == 'bmw':
-		 print(car.upper())
-	 else:
-		 print(car.title())
+#习题7-1
+cars = input("What kind of car do you want to rent?")#input让程序暂停运行，等待用户输入一些文本
+print("Let me see if I can find you a Subaru.")
+#习题7-2
+restaurant = input("How many of you eat?")
+restaurant = int(restaurant)
+if restaurant > 8:
+    print("There are no vacant tables.")
+else:
+    pritn("Free table.")
+#习题7-3
+number = input("Enter a number:")
+number = int(number)
+if number % 10 ==0:
+    print("是10的倍数")
+else:
+    print("不是")
--- parrot.py
+++ parrot.py
@@ -1,3 +1,18 @@
-#函数input()让程序暂停运行，等待用户输入一些文本。获取用户输入后，python将其存储在一个变量中，方便使用
-message = input("Thll me something,and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt +="\nEnter 'quit' to end the program."
+message = "" #定义一个变量message，值为空
+while message != 'quit':#只要message值不是quit，while循环就会不断运行
+    message = input(prompt)#等待用户输入，不管用户输入的什么都将存储到变量message中并打印，只要用户输入的不是‘quit’，
+    # 就会再次显示提示消息并等待用户输入
+if message != 'quit':
+    print(message)
+#使用标志
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt +="\nEnter 'quit' to end the program."
+active = True#把这个标志命名为active（可以指定任何名称），将变量active设置为True，只要变量active为True，循环就继续运行
+while active:
+    message = input(prompt)
+    if message == 'quit':#如果用户输入的是quit，就将变量active设置为False，循环就不再继续执行
+        active = False
+    else:#如果用户输入的不是quit，我们就将输入作为一条消息打印出来
+        print(message)
